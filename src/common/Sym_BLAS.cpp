6ce159935bb49329a38860c7c0082ce2988e10f2
Ben Chung <ckfinite@gmail.com>
2021-05-19 23:33:08 -0400

    use the 32 bit targets

--- src/common/Sym_BLAS.cpp
+++ src/common/Sym_BLAS.cpp
@@ -63,7 +63,7 @@ namespace nasoq {
 #ifdef OPENBLAS
 cblas_dscal(tmp_dim,sca_tmp,tmp1,iun);
 #elif BLAS_JL
- cblas_dscal_64(tmp_dim,sca_tmp,tmp1,iun);
+ cblas_dscal(tmp_dim,sca_tmp,tmp1,iun);
 #else
    SYM_DSCAL(&tmp_dim, &sca_tmp, tmp1, &iun);
 #endif
@@ -84,7 +84,7 @@ cblas_dscal(tmp_dim,sca_tmp,tmp1,iun);
   // dsyr_("L", &dimx, &diag, tmp1, &iun, tmp1_stride, &st); //  ?syr Performs a rank-1 update of a symmetric matrix.
 #elif BLAS_JL
    blasint  st = stride;
-   cblas_dsyr_64(CblasColMajor, CblasLower, dimx, diag, tmp1, iun, tmp1_stride, st); //  ?syr Performs a rank-1 update of a symmetric matrix. 
+   cblas_dsyr(CblasColMajor, CblasLower, dimx, diag, tmp1, iun, tmp1_stride, st); //  ?syr Performs a rank-1 update of a symmetric matrix. 
 #else
    dsyr("L", &dimx, &diag, tmp1, &iun, tmp1_stride, &stride); //  ?syr Performs a rank-1 update of a symmetric matrix.
 #endif
@@ -516,7 +516,7 @@ cblas_dscal(tmp_dim,sca_tmp,tmp1,iun);
 #ifdef OPENBLAS
     cblas_dscal(n_rhs, tmp, rhs + i * lda, iun);
 #elif BLAS_JL
-    cblas_dscal_64(n_rhs, tmp, rhs + i * lda, iun);
+    cblas_dscal(n_rhs, tmp, rhs + i * lda, iun);
 #else
     SYM_DSCAL(&n_rhs, &tmp, rhs + i * lda, &iun);
 #endif
@@ -536,8 +536,8 @@ cblas_dscal(tmp_dim,sca_tmp,tmp1,iun);
     cblas_dscal(n_rhs, one_over_det, rhs + i * lda, iun);
     cblas_dscal(n_rhs, one_over_det, rhs + (i + 1) * lda, iun);
 #elif BLAS_JL
-    cblas_dscal_64(n_rhs, one_over_det, rhs + i * lda, iun);
-    cblas_dscal_64(n_rhs, one_over_det, rhs + (i + 1) * lda, iun);
+    cblas_dscal(n_rhs, one_over_det, rhs + i * lda, iun);
+    cblas_dscal(n_rhs, one_over_det, rhs + (i + 1) * lda, iun);
 #else
     SYM_DSCAL(&n_rhs, &one_over_det, rhs + i * lda, &iun);
     SYM_DSCAL(&n_rhs, &one_over_det, rhs + (i + 1) * lda, &iun);
@@ -583,8 +583,8 @@ cblas_dscal(tmp_dim,sca_tmp,tmp1,iun);
     cblas_dscal(n_rhs, one_over_det, rhs + i * lda, iun);
     cblas_dscal(n_rhs, one_over_det, rhs + (i + 1) * lda, iun);
 #elif BLAS_JL
-    cblas_dscal_64(n_rhs, one_over_det, rhs + i * lda, iun);
-    cblas_dscal_64(n_rhs, one_over_det, rhs + (i + 1) * lda, iun);
+    cblas_dscal(n_rhs, one_over_det, rhs + i * lda, iun);
+    cblas_dscal(n_rhs, one_over_det, rhs + (i + 1) * lda, iun);
 #else
     SYM_DSCAL(&n_rhs, &one_over_det, rhs + i * lda, &iun);
     SYM_DSCAL(&n_rhs, &one_over_det, rhs + (i + 1) * lda, &iun);
